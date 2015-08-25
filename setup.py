4464db87f89f11e8559a10d9a800863d061e7353
Wes Young <wes@barely3am.com>
2015-08-25 15:11:15 -0400

    circle

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
-from os.path import relpath
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
+
 
 def read(*names, **kwargs):
     return io.open(
@@ -63,4 +63,5 @@ setup(
             'cgmail-cif = cgmail.cif:main'
         ]
     },
+    test_suite="test"
 )
